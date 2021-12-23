92111b94be5ec04f6ebdcbf42cb127b1404e5d4a
Utkarsh <saxenautkarsh0@gmail.com>
2021-12-23 08:41:49 +0530

    Configure tailwind and daisyui, remove unnecessary boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    styled: true,
+    themes: [
+      'dark', // first one will be the default theme
+      'emerald',
+      'forest',
+      'synthwave',
+    ],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+  },
+};
